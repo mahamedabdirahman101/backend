3f8198cd2558006c6b630b457ec33573782b7a92
mohamedsaed <mahamedabdirahman413@gmail.com>
2026-02-04 10:18:38 +0300

    commiting after dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"
+import dotenv from "dotenv" 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`The server is running at http://localhost:${PORT}`);
 });
