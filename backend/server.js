333de211fbcaa0b518f26ed7968920f54ecd163d
Mohamed Abdirahman <mahamedabdirahman413@gmail.com>
2026-02-14 11:27:12 +0300

    rev 2

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
 const app = express();
-app.listen(3000, () => {
-  console.log(`Server is running at http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`The server is running at http://localhost:${PORT}`);
 });
+
+//routes
+app.get("/", (req, res) => res.send({ message: "API IS WORKING" }));
+
+//get all books
+app.get("/api/books", (req, res) => res.send({ message: "All Books" }));
+
+//get single book - fetch
+app.get("/api/books/:id", (req, res) => res.send({ message: "Single Book" }));
+
+//post - Save a Book
+app.post("/api/books", (req, res) => res.send({ message: "Save a book" }));
+
+//update - Update a Book
+app.put("/api/books/:id", (req, res) => res.send({ message: "Update a book" }));
+
+//DELETE - Delete a Book
+app.delete("/api/books/:id", (req, res) =>
+  res.send({ message: "Delete a book" }),
+);
